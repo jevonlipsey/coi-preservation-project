48e9724195c587fdad78d8db56540e2626d751f0
NoeCrab <noedeltoro10@gmail.com>
2026-07-20 09:52:15 -0600

    -- added parameter to test state senate and state house partitions

--- run_experiments.py
+++ run_experiments.py
@@ -9,135 +9,33 @@ TARGET_STATES = ["co"]
 
 STATE_CONFIG = {
     "mo": {"notebook": "mo/mo_scoring.ipynb", "state_name": "missouri"},
-    "co": {"notebook": "co/co_scoring.ipynb", "state_name": "colorado"},
+    "co": {"notebook": "co/co_scoring.ipynb", "state_name": "colorado"}, 
 }
 
 experiments = [
     {
-        "accept": "proportional_50",
+        "accept": "optimized_tcp",
         "run_id": 1,
         "weights": "test_weights",
-        "steps": 100_000,
-        "desired_tcp": 0.85,
+        "steps": 500_000,
+        "desired_tcp": 0.8,
+        "dist_lev": "cog" #district level: cog, ss, sh
     },
     {
-        "accept": "proportional_50",
-        "run_id": 2,
-        "weights": "test_weights",
-        "steps": 100_000,
-        "desired_tcp": 0.85,
-    },
-    {
-        "accept": "proportional_100",
+        "accept": "optimized_tcp",
         "run_id": 1,
         "weights": "test_weights",
-        "steps": 100_000,
-        "desired_tcp": 0.85,
+        "steps": 500_000,
+        "desired_tcp": 0.6,
+        "dist_lev": "ss"
     },
     {
-        "accept": "proportional_100",
-        "run_id": 2,
-        "weights": "test_weights",
-        "steps": 100_000,
-        "desired_tcp": 0.85,
-    },
-    {
-        "accept": "proportional_200",
+        "accept": "optimized_tcp",
         "run_id": 1,
         "weights": "test_weights",
-        "steps": 100_000,
-        "desired_tcp": 0.85,
-    },
-    {
-        "accept": "proportional_200",
-        "run_id": 2,
-        "weights": "test_weights",
-        "steps": 100_000,
-        "desired_tcp": 0.85,
-    },
-    {
-        "accept": "margin_10_40",
-        "run_id": 1,
-        "weights": "test_weights",
-        "steps": 100_000,
-        "desired_tcp": 0.85,
-    },
-    {
-        "accept": "margin_10_40",
-        "run_id": 2,
-        "weights": "test_weights",
-        "steps": 100_000,
-        "desired_tcp": 0.85,
-    },
-    {
-        "accept": "margin_20_50",
-        "run_id": 1,
-        "weights": "test_weights",
-        "steps": 100_000,
-        "desired_tcp": 0.85,
-    },
-    {
-        "accept": "margin_20_50",
-        "run_id": 2,
-        "weights": "test_weights",
-        "steps": 100_000,
-        "desired_tcp": 0.85,
-    },
-    {
-        "accept": "margin_30_60",
-        "run_id": 1,
-        "weights": "test_weights",
-        "steps": 100_000,
-        "desired_tcp": 0.85,
-    },
-    {
-        "accept": "margin_30_60",
-        "run_id": 2,
-        "weights": "test_weights",
-        "steps": 100_000,
-        "desired_tcp": 0.85,
-    },
-    {
-        "accept": "simple_25",
-        "run_id": 1,
-        "weights": "test_weights",
-        "steps": 100_000,
-        "desired_tcp": 0.85,
-    },
-    {
-        "accept": "simple_25",
-        "run_id": 2,
-        "weights": "test_weights",
-        "steps": 100_000,
-        "desired_tcp": 0.85,
-    },
-    {
-        "accept": "simple_50",
-        "run_id": 1,
-        "weights": "test_weights",
-        "steps": 100_000,
-        "desired_tcp": 0.85,
-    },
-    {
-        "accept": "simple_50",
-        "run_id": 2,
-        "weights": "test_weights",
-        "steps": 100_000,
-        "desired_tcp": 0.85,
-    },
-    {
-        "accept": "simple_75",
-        "run_id": 1,
-        "weights": "test_weights",
-        "steps": 100_000,
-        "desired_tcp": 0.85,
-    },
-    {
-        "accept": "simple_75",
-        "run_id": 2,
-        "weights": "test_weights",
-        "steps": 100_000,
-        "desired_tcp": 0.85,
+        "steps": 500_000,
+        "desired_tcp": 0.575,
+        "dist_lev": "sh"
     },
 ]
 
@@ -160,17 +58,18 @@ def run_experiment(job):
     # stagger runs
     time.sleep(exp["run_id"] * 2)
 
-    csv_name = f"{config['state_name']}_{exp['accept']}_{exp['weights']}_{exp['steps']}_v{exp['run_id']}"
+    csv_name = f"{config['state_name']}_{exp['dist_lev']}_{exp['accept']}_{exp['weights']}_{exp['steps']}_v{exp['run_id']}"
 
     pm.execute_notebook(
         config["notebook"],  # template
-        f"{results_dir}/{state}_{exp['accept']}_v{exp['run_id']}.ipynb",  # output
+        f"{results_dir}/{state}_{exp['accept']}_{exp['dist_lev']}_v{exp['run_id']}.ipynb",  # output
         parameters=dict(
             STATE_NAME=config["state_name"],
             ACCEPT_STRATEGY_NAME=exp["accept"],
             WEIGHTS_FILE=exp["weights"],
             MARKOV_STEPS=exp["steps"],
             DESIRED_TCP=exp["desired_tcp"],
+            DIST_LEVEL = exp["dist_lev"],
             CSV_FILENAME=csv_name,
         ),
         cwd=state,
